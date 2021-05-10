b28188586ddaf7bf1dc783190ed48bc631c25a23
Muhammad Rizki <rizkielshirazy@gmail.com>
2021-05-10 12:06:43 +0700

    add styling at dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container main-container">
     <div class="row justify-content-center">
         <div class="col-md-12">
-            <div class="card shadow-sm">
+            <div class="card py-3" style="background-color: transparent; border:none">
                 <div class="card-body">
 
                     @php
@@ -13,7 +13,7 @@
 
                     <div class="row">
                         <div class="col-md-4">
-                            <div class="card bg-info text-white">
+                            <div class="card bg-info text-white shadow">
                                 <div class="card-body">
                                     <span>Surat Keluar Tahun Ini</span>
                                     <h3 class="mt-2">{{ App\Outbox::whereYear('date', date('Y'))->count() }}</h3>
@@ -22,7 +22,7 @@
                         </div>
 
                         <div class="col-md-4">
-                            <div class="card bg-secondary text-white">
+                            <div class="card bg-secondary text-white shadow">
                                 <div class="card-body">
                                     <span>Surat Keluar Seluruhnya</span>
                                     <h3 class="mt-2">{{ App\Outbox::count() }}</h3>
@@ -31,7 +31,7 @@
                         </div>
 
                         <div class="col-md-4">
-                            <div class="card bg-warning">
+                            <div class="card bg-warning shadow">
                                 <div class="card-body">
                                     <span>Surat Keluar Tanpa Salinan</span>
                                     <h3 class="mt-2">{{ App\Outbox::whereNull('document')->count() }}</h3>
@@ -43,7 +43,8 @@
 
                     <div class="row mt-5">
                         <div class="col-md-12">
-                            <a href="{{ route('outbox.create') }}" class="btn btn-success px-4 my-3 float-right ">
+                            <a href="{{ route('outbox.create') }}"
+                               class="btn btn-success px-4 my-3 float-right shadow">
                                 <i class="fas fa-plus-circle mr-3"></i> Tambah Surat Keluar</a>
                         </div>
 
@@ -53,32 +54,36 @@
                             $outboxes = App\Outbox::latest()->limit(5)->get();
                             @endphp
 
-                            <h4 class="mb-3">5 Surat Keluar Terakhir</h4>
-                            <table class="table">
-                                <thead>
-                                    <tr>
-                                        <th class="text-center">Nomor Surat</th>
-                                        <th class="text-center">Tanggal</th>
-                                        <th class="text-center">Perihal</th>
-                                        <th class="text-center">Tujuan</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @if (count($outboxes) > 0)
-                                    @foreach ($outboxes as $item)
-                                    <tr>
-                                        <td>{{ $item->reff }}</td>
-                                        <td class="text-center">
-                                            {{ Carbon\Carbon::parse($item->date)->format('d-m-Y') }}
-                                        </td>
-                                        <td>{{ $item->subject }}</td>
-                                        <td>{{ $item->destination }}</td>
-                                    </tr>
-                                    @endforeach
-                                    @else
-                                    @endif
-                                </tbody>
-                            </table>
+                            <div class="card shadow">
+                                <div class="card-body">
+                                    <h4 class="mb-3">5 Surat Keluar Terakhir</h4>
+                                    <table class="table">
+                                        <thead>
+                                            <tr>
+                                                <th class="text-center">Nomor Surat</th>
+                                                <th class="text-center">Tanggal</th>
+                                                <th class="text-center">Perihal</th>
+                                                <th class="text-center">Tujuan</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @if (count($outboxes) > 0)
+                                            @foreach ($outboxes as $item)
+                                            <tr>
+                                                <td>{{ $item->reff }}</td>
+                                                <td class="text-center">
+                                                    {{ Carbon\Carbon::parse($item->date)->format('d-m-Y') }}
+                                                </td>
+                                                <td>{{ $item->subject }}</td>
+                                                <td>{{ $item->destination }}</td>
+                                            </tr>
+                                            @endforeach
+                                            @else
+                                            @endif
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
 
 
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,9 +58,7 @@
         .is-invalid~.select2-container .select2-selection--single {
             border-color: #e55353;
         }
-    </style>
 
-    <style>
         @import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
 
         .navbar-1-1.navbar-light .navbar-nav .nav-link {
@@ -71,8 +69,22 @@
         .navbar-1-1.navbar-light .navbar-nav .nav-link.active {
             font-weight: 500;
         }
+
+        .main-container {
+            box-shadow: 0 0 1rem 0 rgba(0, 0, 0, .2);
+            border-radius: 5px;
+            background-color: rgba(255, 255, 255, .15);
+
+            backdrop-filter: blur(5px);
+        }
     </style>
 
+    <style>
+        body {
+            background: #D6D8D5 url("{{ asset('images/background.jpg') }}") no-repeat;
+            background-size: cover;
+        }
+    </style>
 </head>
 
 <body>
