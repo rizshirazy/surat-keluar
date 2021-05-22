cd85d24952e74d90b1a45434b9bbb69cc65ea26b
Muhammad Rizki <rizkielshirazy@gmail.com>
2021-05-22 17:02:57 +0700

    add footer and protection on excel export

--- app/Exports/OutboxesExport.php
+++ app/Exports/OutboxesExport.php
@@ -5,24 +5,29 @@ namespace App\Exports;
 use App\Outbox;
 use Illuminate\Contracts\Support\Responsable;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\URL;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromView;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithColumnFormatting;
+use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Concerns\WithStyles;
+use Maatwebsite\Excel\Events\AfterSheet;
+use Maatwebsite\Excel\Events\BeforeExport;
 use Maatwebsite\Excel\Excel;
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Style\Alignment;
 use PhpOffice\PhpSpreadsheet\Style\Border;
 use PhpOffice\PhpSpreadsheet\Style\Fill;
 use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
+use PhpOffice\PhpSpreadsheet\Style\Protection;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
 class OutboxesExport implements
     FromView,
     Responsable,
-    WithColumnFormatting,
     ShouldAutoSize,
+    WithColumnFormatting,
+    WithEvents,
     WithStyles
 {
     use Exportable;
@@ -44,13 +49,47 @@ class OutboxesExport implements
         $this->end_date = $end_date;
     }
 
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function (AfterSheet $event) {
+                $last_column = $event->sheet->getHighestDataColumn();
+
+                // calculate last row + 2 (total results + new row)
+                $last_row = $event->sheet->getHighestDataRow() + 2;
+
+                // set up a style array for cell formatting
+                $style_text_center = [
+                    'font' => [
+                        'italic' => true,
+                    ],
+                    'alignment' => [
+                        'horizontal' => Alignment::HORIZONTAL_CENTER
+                    ]
+                ];
+
+                $event->sheet->mergeCells(sprintf('A%d:%s%d', $last_row, $last_column, $last_row));
+                $event->sheet->setCellValue(sprintf('A%d', $last_row), env('APP_NAME', 'SISUAR') . ' ' . URL::to('/') . ' | ' . env('APP_DEVELOPER', 'Muhammad Rizki, S.Si') . ' - 2021');
+                $event->sheet->getStyle(sprintf('A%d', $last_row))->applyFromArray($style_text_center);
+            },
+            BeforeExport::class => function (BeforeExport $event) {
+                $event->writer->getDelegate()->getSecurity()->setLockWindows(true);
+                $event->writer->getDelegate()->getSecurity()->setLockStructure(true);
+                $event->writer->getDelegate()->getSecurity()->setWorkbookPassword("admin");
+                $event->writer->getProperties()->setCreator(env('APP_DEVELOPER', 'Muhammad Rizki, S.Si'));
+            }
+        ];
+    }
+
     /**
      * @return \Illuminate\Support\Collection
      */
     public function view(): View
     {
+        $data = Outbox::whereYear('date', date('Y'));
+
         return view('exports.outboxes', [
-            'data' => Outbox::all()
+            'data' => $data
         ]);
     }
 
@@ -65,6 +104,12 @@ class OutboxesExport implements
     {
 
         $highestRow = $sheet->getHighestDataRow();
+        $protection = $sheet->getProtection();
+        $protection->setPassword('admin');
+        $protection->setSheet(true);
+        $protection->setSort(true);
+        $protection->setInsertRows(true);
+        $protection->setFormatCells(true);
 
         return [
             1   =>
